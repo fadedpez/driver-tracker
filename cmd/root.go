3f2e569dc0b00e8623e9bfe8dacac6836c45fed6
Kirk Gilmore <kirklgilmore@gmail.com>
2021-12-18 19:34:16 -0800

    add newDynamoClient func

--- cmd/root.go
+++ cmd/root.go
@@ -3,11 +3,30 @@ package cmd
 import (
 	"log"
 
+	"github.com/KirkDiggler/go-projects/dynamo"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 	"github.com/spf13/cobra"
 )
 
 var rootCommand = &cobra.Command{
 	Use: "driver-tracker",
+}
+
+func newDynamoClient() *dynamo.Client {
+	endpoint := "http://localhost:8000"
+	region := "us-east-1"
+
+	awsClient := dynamodb.New(dynamodb.Options{
+		Region:           region,
+		EndpointResolver: dynamodb.EndpointResolverFromURL(endpoint),
+	})
+
+	client, err := dynamo.NewClient(&dynamo.ClientConfig{AWSClient: awsClient})
+	if err != nil {
+		log.Fatal("err returned from dynamo.NewClient. ", err)
+	}
+
+	return client
 }
 
 func Execute() {
